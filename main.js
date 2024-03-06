df45a803ec8a25442c6316f632b3cda76d3e3762
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-06 16:04:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+// Personal Message printing
+var name01 = 'Asif';
+console.log("".concat(name01, ", would you like to learn some typescript today?"));
---
+++ main.ts
@@ -0,0 +1,5 @@
+// Personal Message printing
+
+let name01: string ='Asif';
+
+console.log(`${name01}, would you like to learn some typescript today?`);
